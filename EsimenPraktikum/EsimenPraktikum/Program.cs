74b8570fadb933fd9b5d32595a279782c50ec285
Siim Kallari <skallari@itcollege.ee>
2016-09-25 11:30:04 +0300

    Asking user input until user gives integer

---
+++ EsimenPraktikum/EsimenPraktikum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsimenePraktikum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kysimus = "Palun anna üks arv: ";
+
+            int a = kysiKasutajaltArvu(kysimus);
+
+            Console.WriteLine("Sisestatud arv oli: "+a);
+        }
+
+        static int kysiKasutajaltArvu(string kysimus)
+        {
+            int a;
+
+            Console.Write(kysimus);
+            string vastus = Console.ReadLine();
+            
+            if (int.TryParse(vastus, out a))
+            {
+                return a;
+            }
+            else
+            {
+                return kysiKasutajaltArvu(kysimus);
+            }
+        }
+    }
+}
